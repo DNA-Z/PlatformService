95fc44443cfff80e8ee13394896456b06239d6db
Inna Borodina <creator.geek@gmail.com>
2023-05-06 13:59:06 +0300

    add dbcontext and repository

---
+++ PlatformService.API/Data/IPlatformRepo.cs
@@ -0,0 +1,14 @@
+﻿using PlatformService.API.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.API.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
